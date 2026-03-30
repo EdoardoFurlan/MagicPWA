00be2925f2dea28a5ebea75d3576a02b74488f51
Edoardo Furlan <edoardofurlan23@gmail.com>
2026-03-30 22:28:31 +0200

    image api

--- backend/app/receipts/router.py
+++ backend/app/receipts/router.py
@@ -3,7 +3,7 @@ from app.receipts.schemas import ReceiptResponse
 from app.receipts.service import process_receipt
 
 
-router = APIRouter(tags=["receipts"])
+router = APIRouter(prefix="/api", tags=["receipts"])
 
 
 @router.post("/upload-receipt/", response_model=ReceiptResponse)
